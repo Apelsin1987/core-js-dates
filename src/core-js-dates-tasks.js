abe2c72922a4556cae8950f7c89b8cb71981830a
Konstantin Si <apelsin1987@gmail.com>
2024-03-16 18:34:01 +0100

    feat: solve isDateInPeriod, formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -128,8 +128,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const currentDate = new Date(date);
+  return (
+    currentDate >= new Date(period.start) && currentDate <= new Date(period.end)
+  );
 }
 
 /**
@@ -143,8 +146,16 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'UTC',
+  }).format(new Date(date));
 }
 
 /**
