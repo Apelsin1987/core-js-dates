857c5458257dff9224e9f62dbe8db366b692d95c
Konstantin Si <apelsin1987@gmail.com>
2024-03-17 17:12:23 +0100

    feat: solve getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -253,8 +253,20 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  switch (true) {
+    case month < 3:
+      return 1;
+    case month < 6:
+      return 2;
+    case month < 9:
+      return 3;
+    case month < 12:
+      return 4;
+    default:
+      return undefined;
+  }
 }
 
 /**
